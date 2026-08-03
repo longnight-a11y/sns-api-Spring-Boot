346eed78bd8369bce5caaff1ed1a2beb621c0e84
Mirai <LONG_NOGHT@icloud.com>
2026-08-03 19:31:32 +0900

    first commit

---
+++ src/main/java/com/example/snsapi/SnsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.snsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApiApplication.class, args);
+	}
+
+}
