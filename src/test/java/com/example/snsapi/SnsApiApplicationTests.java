af93ae68eff6058d5f76f34ac4f439786d605999
Mirai <LONG_NOGHT@icloud.com>
2026-08-03 19:37:45 +0900

    Fix ci.yml

--- src/test/java/com/example/snsapi/SnsApiApplicationTests.java
+++ src/test/java/com/example/snsapi/SnsApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SnsApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
