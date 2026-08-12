4ccc879422921accf45e0e531f1bc763d0ab0392
Mirai <LONG_NOGHT@icloud.com>
2026-08-12 16:37:01 +0900

    feat: add JWT and its related exception handler

---
+++ src/main/java/com/example/snsapi/dto/ErrorResponse.java
@@ -0,0 +1,6 @@
+package com.example.snsapi.dto;
+
+public record ErrorResponse(
+        String message
+) {
+}
---
+++ src/main/java/com/example/snsapi/service/JwtAuthFilter.java
@@ -0,0 +1,46 @@
+package com.example.snsapi.service;
+
+import com.example.snsapi.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.UUID;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtAuthFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException{
+        String header = request.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer ")){
+            String token = header.substring(7);
+            UUID userId = jwtService.decodeToken(token);
+            if(userId != null){
+                userRepository.findById(userId).ifPresent(user -> {
+                    var auth = new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                });
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
